19d2ffb875340e5ad3f94cf7950a80387d8863e6
Laurent DAVERIO <laurent@daverio.net>
2020-05-15 16:06:26 +0200

    Ajout `argparse` sur le script `script_essai`

--- mon_autre_package/scripts/script_essai.py
+++ mon_autre_package/scripts/script_essai.py
@@ -2,6 +2,23 @@
 Exemple de script en ligne de commande
 """
 
+import argparse
+
+from mon_package import Boat, Simulator
+
 
 def main():
-    print('Hello script')
+    """
+    Lancement d'un Simulator en ligne de commande
+    :return:
+    """
+    # Traitement des arguments
+    parser = argparse.ArgumentParser(description='Process arguments.')
+    parser.add_argument('boat_name', type=str,
+                        help='nom du bateau')
+
+    args = parser.parse_args()
+
+    b = Boat(args.boat_name)
+    s = Simulator('simu', b)
+    print(s)
